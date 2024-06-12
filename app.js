7ca33880bcd7c0d65ae001350f2afaf3429dad4a
MuhammadAsif46 <asifahmed32c@gmail.com>
2024-06-13 02:08:13 +0500

    added bg sound

--- app.js
+++ app.js
@@ -1,4 +1,22 @@
-var audio = document.createElement('audio');
-audio.setAttribute('src', 'sound.mp3'); 
-audio.loop=true;
-audio.play();
+let audio = document.createElement('audio');
+        audio.src = 'sound.mp3';
+        audio.loop = true;
+
+        // Function to play the audio
+        function playAudio() {
+            audio.play().then(() => {
+                console.log('Audio is playing');
+            }).catch(function(error) {
+                console.log('Playback prevented:', error);
+            });
+
+            // Remove the event listener after the audio starts playing
+            document.removeEventListener('click', playAudio);
+        }
+
+        // Add event listener for any click on the document
+        document.addEventListener('click', playAudio);
+
+        audio.addEventListener('error', function(e) {
+            console.log('Error loading audio file:', e);
+        });
